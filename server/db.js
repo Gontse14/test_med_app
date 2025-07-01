afada592721960088c1b2c27ce91c4b726db3b90
Gontse14 <mapulabopape53@gmail.com>
2025-07-01 16:55:02 -0400

    db

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:4NbQOdMLYUU971En3KHregQc@172.21.168.108:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
